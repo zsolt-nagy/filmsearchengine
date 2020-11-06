d4cb86bde6cdc7a3a11315e91ca0c420c8f6a15d
raisgood <rashadhumphrey@hotmail.com>
2020-11-05 19:05:40 -0600

    update

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,21 @@
 $(document).ready(() => {
-    $('#searchForm').submit( (e) => {
+    $('#searchForm').submit((e) => {
         e.preventDefault();
         let searchText = $('#searchText').val();
         getMovies(searchText);
     });
 });
-
-function getMovies(searchText){
-    var API_URL = `http://www.omdbapi.com/?s=${searchText}&apikey=387cdace`;  // 'http://www.omdbapi.com/?i=tt3896198&apikey=387cdace'
+//"https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json" "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json" "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json" "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json" "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json" "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json" "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json"
+function getMovies(searchText) {
+    var API_URL = `http://www.omdbapi.com/?s=${searchText}&apikey=387cdace`; // 'http://www.omdbapi.com/?i=tt3896198&apikey=387cdace'
     axios.get(API_URL)
         .then((response) => {
-        console.log(response);
-        
-        let output = '';
-        let movies = response.data.Search;
-           $.each(movies, (index, movie) => {
-            output +=`
+            console.log(response);
+
+            let output = '';
+            let movies = response.data.Search;
+            $.each(movies, (index, movie) => {
+                output += `
             <div class="col-md-3">
             <div class="well text-center">
             <img src="${movie.Poster}">
@@ -24,43 +24,44 @@ function getMovies(searchText){
             </div>
             </div>
             `;
+            });
+
+            /*
+                        let movie = response.data;
+                        output += `
+                            <div class="col-md-3">
+                            <div class="well text-center">
+                            <img src="${movie.Poster}">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                            </div>
+                            </div>
+                            `;
+            */
+
+
+            $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
         });
-        /*
-        let movie = response.data;
-        output +=`
-            <div class="col-md-3">
-            <div class="well text-center">
-            <img src="${movie.Poster}">
-            <h5>${movie.Title}</h5>
-            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-            </div>
-            </div>
-            `;
-
-        */
-
-$('#movies').html(output);
-})
-.catch((err) => {
-console.log(err);
-});
 }
 
 function movieSelected(id) {
-sessionStorage.setItem('movieId', id);
-window.location = 'movie.html';
-return true;
+    localStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return true;
 }
 
 function getMovie() {
-let movieId = sessionStorage.getItem('searchText');
-var API_URL = `http://www.omdbapi.com/?s=${movieId}&apikey=387cdace`;  // 'http://www.omdbapi.com/?i=tt3896198&apikey=387cdace'
-axios.get(API_URL)
-.then((response) => {
-console.log(response);
-let movie = response.data.Search;
+    let movieId = localStorage.getItem('searchText');
+    var API_URL = `http://www.omdbapi.com/?s=${movieId}&apikey=387cdace`; //'http://www.omdbapi.com/?i=tt3896198&apikey=387cdace'
+    axios.get(API_URL)
+        .then((response) => {
+            console.log(response);
+            let movie = response.data.Search;
 
-let output = `
+            let output = `
 <div class="row">
 <div class="col-md-4">
 <img src="${movie.Poster}" class="thumbnail">
@@ -84,15 +85,14 @@ let output = `
 <h3>Plot</h3>
 ${movie.Plot}
 <hr>
-<a href="http://imdb.com/title${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+<a href="http://imdb.com" target="_blank" class="btn btn-primary">View IMDB</a>
 <a href="index.html" class="btn btn-default">Go Back To Search</a>
 </div>
 </div>
 `;
 
-$('#movie').html(output);
-})
-.catch((err) => {
-console.log(err);
-});
+            $('#movie').html(output);
+        }).catch((err) => {
+            console.log(err);
+        });
 }
